2a38816477b4dc6c593063af003742de5fbf3d51
Volvick <volvick5050@gmail.com>
2025-05-21 12:38:38 +0530

    repeat number question

--- Array/buySellStock.java
+++ Array/buySellStock.java
@@ -1,18 +1,18 @@
 public class buySellStock {
     public static int buySellStock(int stock[]) {
         int buyPrice = Integer.MAX_VALUE;
-        int maxProfit=0;
+        int maxProfit = 0;
         for (int i = 0; i < stock.length; i++) {
-            if (buyPrice<stock[i]) {
-                int profit = stock[i] - buyPrice;//today's profit
-                maxProfit= Math.max(maxProfit, profit);
-            }
-            else{
-                buyPrice=stock[i];
+            if (buyPrice < stock[i]) {
+                int profit = stock[i] - buyPrice;// today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = stock[i];
             }
         }
         return maxProfit;
     }
+
     public static void main(String[] args) {
         int stock[] = { 7, 10, 1, 3, 6, 4 };
         System.out.println(buySellStock(stock));
