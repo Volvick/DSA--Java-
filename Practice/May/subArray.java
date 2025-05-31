3874d598d0007cb51a4faf252b156d157de523eb
Volvick <volvick5050@gmail.com>
2025-05-31 19:26:50 +0530

    practice

--- Practice/May/subArray.java
+++ Practice/May/subArray.java
@@ -1,20 +1,19 @@
 package May;
 
 public class subArray {
-  public static void subArray(int arr[]) {
+  public static void subArray(int arr[]){
     for (int i = 0; i < arr.length; i++) {
       for (int j = i; j < arr.length; j++) {
-        for (int j2 = i; j2 <= j; j2++) {
-          System.out.print(arr[j2] + " ");
+        for (int j2 = i; j2 <=j; j2++) {
+          System.out.print(arr[j2]+" ");
         }
         System.out.println();
       }
       System.out.println();
     }
   }
-
   public static void main(String[] args) {
-    int arr[] = { 2, 4, 6, 8, 10 };
+    int arr[] = {2,4,5,3,9,8,7};
     subArray(arr);
   }
 }
