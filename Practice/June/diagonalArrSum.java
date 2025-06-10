5b0b310b7198af9ce1c6e6fc566019a1c395306b
Volvick <volvick5050@gmail.com>
2025-06-10 20:28:19 +0530

    removed magic number

--- Practice/June/diagonalArrSum.java
+++ Practice/June/diagonalArrSum.java
@@ -3,17 +3,23 @@ package June;
 public class diagonalArrSum {
   public static void diagonalArrSum(int matrix[][]) {
     int sum = 0;
-    for (int i = 0; i < matrix.length; i++) {
-      sum += matrix[i][i];
-      if (i != matrix.length - i - 1) {
-        sum += matrix[i][matrix.length - i - 1];
+    int n = matrix.length;
+    for (int i = 0; i < n; i++) {
+      sum += matrix[i][i]; // primary diagonal
+      if (i != n - i - 1) {
+        sum += matrix[i][n - i - 1]; // secondary diagonal
       }
     }
     System.out.println("Sum: " + sum);
   }
 
   public static void main(String[] args) {
-    int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+    int matrix[][] = {
+      { 1, 2, 3, 4 },
+      { 5, 6, 7, 8 },
+      { 9, 10, 11, 12 },
+      { 13, 14, 15, 16 }
+    };
     diagonalArrSum(matrix);
   }
 }
