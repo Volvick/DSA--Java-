28584e39c6014c8da36315871c25bf9617691065
Volvick <volvick5050@gmail.com>
2025-06-10 18:54:48 +0530

    practice diagonal sum

---
+++ Practice/June/diagonalArrSum.java
@@ -0,0 +1,19 @@
+package June;
+
+public class diagonalArrSum {
+  public static void diagonalArrSum(int matrix[][]) {
+    int sum = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      sum += matrix[i][i];
+      if (i != matrix.length - i - 1) {
+        sum += matrix[i][matrix.length - i - 1];
+      }
+    }
+    System.out.println("Sum: "+sum);
+  }
+
+  public static void main(String[] args) {
+    int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+    diagonalArrSum(matrix);
+  }
+}
