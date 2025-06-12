52cc004c66b35b9f2fb1e34ba5c446433bdf1b7c
Volvick <volvick5050@gmail.com>
2025-06-12 05:53:38 +0530

    transpose matrix practice

--- 2DArray/Practice/transpose.java
+++ 2DArray/Practice/transpose.java
@@ -2,9 +2,18 @@ package Practice;
 
 public class transpose {
   public static void transpose(int arr[][]){
+    int newArr[][] = new int[arr[0].length][arr.length];
     for (int i = 0; i < arr.length; i++) {
       for (int j = 0; j < arr[0].length; j++) {
+        newArr[j][i] = arr[i][j];
         System.out.print(arr[i][j]+" ");
+      }
+      System.out.println();
+    }
+    System.out.println("----------------------");
+    for (int i = 0; i < newArr.length; i++) {
+      for (int j = 0; j < newArr[0].length; j++) {
+        System.out.print(newArr[i][j]+" ");
       }
       System.out.println();
     }
