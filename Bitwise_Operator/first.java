0fd25d0e814efb86d13583a6ec18bd39337d99aa
Volvick <volvick5050@gmail.com>
2025-06-15 19:48:16 +0530

    started bitwise operator

---
+++ Bitwise_Operator/first.java
@@ -0,0 +1,12 @@
+package Bitwise_Operator;
+
+public class first {
+  public static void main(String[] args) {
+    System.out.println(5>>1);
+    System.out.println(5&6);
+    System.out.println(5|6);
+    System.out.println(5^6);
+    System.out.println(~3);
+    System.out.println(5<<1);
+  }
+}
