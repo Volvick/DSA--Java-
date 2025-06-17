f6cb8e09df653407e49829b7b96d88c945817632
Volvick <volvick5050@gmail.com>
2025-06-18 04:31:36 +0530

    butterfly

---
+++ Practice/June/butterly.java
@@ -0,0 +1,33 @@
+package June;
+
+public class butterly {
+  public static void main(String[] args) {
+    int n=5;
+    //for upper part
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <=i; j++) {
+        System.out.print("* ");
+      }
+      for (int j = 1; j <= 2*(n-i); j++) {
+        System.out.print("  ");
+      }
+      for (int j = 1; j <=i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+    //for bottom part
+    for (int i = 5; i >0 ; i--) {
+      for (int j = 1; j <=i; j++) {
+        System.out.print("* ");
+      }
+      for (int j = 1; j <= 2*(n-i); j++) {
+        System.out.print("  ");
+      }
+      for (int j = 1; j <=i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
