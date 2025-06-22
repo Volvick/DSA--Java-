a05fa50aaad58df4f6ec6ebb6cb9730a798d7780
Volvick <volvick5050@gmail.com>
2025-06-22 21:29:28 +0530

    constructor

--- Class_Object/constructor.java
+++ Class_Object/constructor.java
@@ -2,27 +2,32 @@ package Class_Object;
 
 public class constructor {
   public static void main(String[] args) {
-    Student s1 = new Student();                  // prints: Default constructor
-    Student s2 = new Student("Volvick");         // prints: Name: Volvick
-    Student s3 = new Student(50);                // prints: Roll: 50
+    Student s1 = new Student();
+    s1.marks[0] = 100;
+    s1.marks[1] = 88;
+    s1.marks[2] = 99;
+    Student s2 = new Student(s1);
+    s1.marks[2] = 39;
+    for (int i = 0; i < 3; i++) {
+      System.out.println(s2.marks[i]);
+    }
   }
 }
 
 class Student {
   String name;
   int roll;
+  int marks[];
+
+  Student(Student s1) {
+    marks = new int[3];
+    this.name = s1.name;
+    this.roll = s1.roll;
+    this.marks = s1.marks;
+  }
 
   Student() {
+    marks = new int[3];
     System.out.println("Default constructor");
-  }
-
-  Student(String name) {
-    this.name = name;
-    System.out.println("Name: " + name);
-  }
-
-  Student(int roll) {
-    this.roll = roll;
-    System.out.println("Roll: " + roll);
   }
 }
