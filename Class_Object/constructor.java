7f13a3f64e26270f1d3b83f24b023dcf74215e8c
Volvick <volvick5050@gmail.com>
2025-06-23 04:57:51 +0530

    constructor

--- Class_Object/constructor.java
+++ Class_Object/constructor.java
@@ -3,10 +3,12 @@ package Class_Object;
 public class constructor {
   public static void main(String[] args) {
     Student s1 = new Student();
+    s1.password = "abc";
     s1.marks[0] = 100;
     s1.marks[1] = 88;
     s1.marks[2] = 99;
     Student s2 = new Student(s1);
+    s2.password = "xyz";
     s1.marks[2] = 39;
     for (int i = 0; i < 3; i++) {
       System.out.println(s2.marks[i]);
@@ -17,15 +19,24 @@ public class constructor {
 class Student {
   String name;
   int roll;
+  String password;
   int marks[];
-
+  // shallow copy constructor
+  // Student(Student s1) {
+  //   marks = new int[3];
+  //   this.name = s1.name;
+  //   this.roll = s1.roll;
+  //   this.marks = s1.marks;
+  // }
+  //deep copy constructor
   Student(Student s1) {
     marks = new int[3];
     this.name = s1.name;
     this.roll = s1.roll;
-    this.marks = s1.marks;
+    for (int i = 0; i < 3; i++) {
+      this.marks[i] = s1.marks[i];
+    }
   }
-
   Student() {
     marks = new int[3];
     System.out.println("Default constructor");
