f96233b64f57a573c327506e2f1762bb67d2aa7b
Volvick <volvick5050@gmail.com>
2025-07-01 04:59:34 +0530

    last occurance

---
+++ Recursion/Last_Occurance.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class Last_Occurance {
+  static int lastOccurance(int arr[], int key, int i) {
+    // if (i < 0) {
+    // return -1;
+    // }
+    // if (arr[i] == key) {
+    // return i;
+    // }
+    // return lastOccurance(arr, key, i - 1);
+
+    if (i == arr.length) {
+      return -1;
+    }
+    int isFound = lastOccurance(arr, key, i + 1);
+    if (isFound == -1 && arr[i] == key) {
+      return i;
+    }
+    return isFound;
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 1, 3, 4, 2, 4,5,3,56 };
+    int n = arr.length - 1;
+    System.out.println(lastOccurance(arr, 4, n));
+  }
+}
