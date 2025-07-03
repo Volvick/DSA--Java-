cb9a94a60f426f5f03b1df278a510c8f1c9b8017
Volvick <volvick5050@gmail.com>
2025-07-03 23:57:45 +0530

    power calc

---
+++ Recursion/OptimizedPower.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class OptimizedPower {
+  static int optimizedPower(int a, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    int halfPower = optimizedPower(a, n / 2);
+    int halfPowerSquare = halfPower * halfPower;
+    if (n % 2 != 0) {
+      return a * halfPowerSquare;
+    }
+    return halfPowerSquare;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(optimizedPower(2, 10));
+  }
+}
